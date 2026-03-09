982b66c395e2126d12a2a5fa9cb038e8a5ef4585
PaoloMucedero <p.mucedero@gmail.com>
2026-03-09 12:16:01 +0100

    fix db connection

--- db/dbConnection.js
+++ db/dbConnection.js
@@ -1,10 +1,12 @@
 const mysql = require(`mysql2`);
+require('dotenv').config();
 
 const dbConnection = mysql.createConnection({
-    host: `localhost`,
-    user: `root`,
-    password: `Scarlett.212!`,
-    database: `boolzip_shop`,
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT || 3306,
 });
 
 dbConnection.connect((err) => {
