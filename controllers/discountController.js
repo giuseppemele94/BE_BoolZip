9961a1b62893f11cae0ad1cdc5ba148ffef6f21c
PaoloMucedero <p.mucedero@gmail.com>
2026-03-10 16:40:06 +0100

    aggiunti commenti codice

--- controllers/discountController.js
+++ controllers/discountController.js
@@ -22,13 +22,15 @@ function index(req, res){
 
 
 function checkCoupon(req, res){
+// Funzione che verifica se un coupon inviato dal frontend è valido
 
   const { code } = req.body;
-
+// Recupera il codice coupon dal corpo della richiesta (.body configura coupon dal FE come quelli del DB) 
   const sql = "SELECT * FROM coupons WHERE code = ?";
 
   db.query(sql, [code], (err, results) => {
-
+  // Prepara la query SQL per cercare il coupon nel database
+  // Il ? verrà sostituito in modo sicuro dal valore di code (evita SQL injection)
     if(err){
       console.error(err);
       return res.status(500).json({
@@ -42,10 +44,14 @@ function checkCoupon(req, res){
         message: "Codice sconto non valido"
       });
     }
-
+    // Se la query non ha trovato alcun coupon con quel codice
+    // significa che il codice non esiste → ritorna 404
     const coupon = results[0];
+    // Coupon contiene campi come: code, discount_value, start_date, end_date
 
     const today = new Date();
+    // Crea un oggetto Date con la data e ora correnti sul server
+    // Serve per confrontare se il coupon è valido "oggi"
     const start = new Date(coupon.start_date);
     const end = new Date(coupon.end_date);
     // Converte le date di inizio e fine del coupon (dal DB) in oggetti Date
